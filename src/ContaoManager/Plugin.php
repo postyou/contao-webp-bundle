371e876214f7ed25da3500f331f63aeb0d486420
Mario Gienapp <mario.gienapp@postyou.de>
2019-03-27 10:25:37 +0100

    fix namespace

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\ContaoWebPBundle\ContaoManager;
+namespace Postyou\ContaoWebPBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
